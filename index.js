a436f859be198668343c1f415595a152731bf201
ShreyashKira <keleven64@gmail.com>
2020-10-02 21:17:36 +0530

    adding captcha feature

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Canvacord = require("./src/Canvacord");
-
+const captcha = require('captcha-canvas')
 module.exports = Canvacord;
 module.exports.plugins = require("./src/Plugins");
+module.exports.Captcha = captcha.CaptchaGenerator;
