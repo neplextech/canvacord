e17752f3f6c4b09ecab1c0d51f7f2487f0173198
Snowflake107 <devsnowflake107@gmail.com>
2021-01-07 15:09:30 +0545

    use @canvacord/emoji-parser

--- plugins/renderEmoji.ts
+++ plugins/renderEmoji.ts
@@ -1,5 +1,6 @@
-import emoji from "node-canvas-with-twemoji-and-discord-emoji";
+import { fillTextWithTwemoji } from "@canvacord/emoji-parser";
+import { CanvasRenderingContext2D } from "canvas";
 
-export default async (ctx, message, x, y) => {
-    return await emoji.fillTextWithTwemoji(ctx, message, x, y);
+export default async (ctx: CanvasRenderingContext2D, message: string, x: number, y: number) => {
+    return await fillTextWithTwemoji(ctx, message, x, y);
 };
