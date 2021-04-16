e4a56fa2af185b5554e6d56f721cd917398c9890
Snowflake107 <devsnowflake107@gmail.com>
2021-04-16 23:46:49 +0545

    Restart
    Co-authored-by: OmgImAlexis <OmgImAlexis@users.noreply.github.com>
    
    Co-authored-by: Androz2091 <androz2091@users.noreply.github.com>

--- test/index.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const canvacord = require("../index");
-
-    const img = await canvacord.clyde("😩 What do u want? <:kek:706738061265993831>");
-
-    canvacord.write(img, "clyde.png");
-})();
