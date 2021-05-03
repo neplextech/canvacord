6011a54600b535239ec3dc09027f808fa00eee17
Snowflake107 <devsnowflake107@gmail.com>
2021-05-03 19:31:48 +0545

    export matrix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { CanvacordImgen as ImageMaker } from "./imgen/Imgen";
 export { Photoshop } from "./editor/Photoshop";
 export { Util } from "./Utils/Util";
+export { Matrices } from "./editor/Matrix";
 export * from "./types/globalTypes";
