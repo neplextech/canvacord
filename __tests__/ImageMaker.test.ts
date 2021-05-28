1f534c238d8346ae65b3c27641801471d27fe102
Snowflake107 <devsnowflake107@gmail.com>
2021-05-28 19:07:23 +0545

    prettier

--- __tests__/ImageMaker.test.ts
+++ __tests__/ImageMaker.test.ts
@@ -1,12 +1,12 @@
 import { ImageMaker } from '../src/index';
-import FileType from "file-type";
+import FileType from 'file-type';
 
 const IMAGE_PATH = `${__dirname}/assets/img.jpg`;
 
 describe('Canvacord ImageMaker', () => {
     test('ImageMaker.triggered() should be GIF', () => {
         return ImageMaker.triggered(IMAGE_PATH).then(async (res) => {
-            expect((await (FileType.fromBuffer(res))).mime).toBe('image/gif');
+            expect((await FileType.fromBuffer(res)).mime).toBe('image/gif');
         });
     });
 
@@ -18,7 +18,7 @@ describe('Canvacord ImageMaker', () => {
 
     test('ImageMaker.beautiful() should be png', () => {
         return ImageMaker.beautiful(IMAGE_PATH).then(async (res) => {
-            expect((await (FileType.fromBuffer(res))).mime).toBe('image/png');
+            expect((await FileType.fromBuffer(res)).mime).toBe('image/png');
         });
     });
 });
--- __tests__/Photoshop.test.ts
+++ __tests__/Photoshop.test.ts
@@ -1,19 +1,18 @@
 import { Photoshop } from '../src/index';
-import FileType from "file-type";
+import FileType from 'file-type';
 
 const IMAGE_PATH = `${__dirname}/assets/img.jpg`;
 
 describe('Canvacord Photoshop', () => {
     test('ImageMaker.color() should be PNG', () => {
-        return Photoshop.color("#FF0000", 16, 16).then(async (res) => {
-            expect((await (FileType.fromBuffer(res))).mime).toBe('image/png');
+        return Photoshop.color('#FF0000', 16, 16).then(async (res) => {
+            expect((await FileType.fromBuffer(res)).mime).toBe('image/png');
         });
     });
 
     test('ImageMaker.colorfy() should be PNG', () => {
-        return Photoshop.colorfy(IMAGE_PATH, "#FF0000").then((res) => {
+        return Photoshop.colorfy(IMAGE_PATH, '#FF0000').then((res) => {
             expect(res).toBeInstanceOf(Buffer);
         });
     });
-
 });
