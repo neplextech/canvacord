72df0cc3276646d3bb70748658c8d763535cac91
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-03 09:01:21 +0545

    chore: remove distribution files

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./canvas/BaseCanvas";
-export * from "./canvas/CanvasBuilder2D";
-export * from "./canvas/MemeCanvas";
-export * from "./canvas/UtilityCanvas";
-export * from "./Utils/Util";
--- dist/index.mjs
+++
@@ -1,9 +0,0 @@
-import mod from "./index.js";
-
-export default mod;
-export const BaseCanvas = mod.BaseCanvas;
-export const BuilderReadyState = mod.BuilderReadyState;
-export const CanvasBuilder2D = mod.CanvasBuilder2D;
-export const MemeCanvas = mod.MemeCanvas;
-export const Util = mod.Util;
-export const UtilityCanvas = mod.UtilityCanvas;
