8b53bd448a0db48cbb3cb5641c3fb99071c0d6f2
Andromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-09-19 08:21:12 +0545

    refactor(Singleton): use symbol

--- src/decorators/Singleton.ts
+++ src/decorators/Singleton.ts
@@ -1,15 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+const InstanceK = Symbol("instance");
+
 type SingletonClass<T extends new (...args: any[]) => any> = T & {
-    instance: T extends new (...args: any[]) => infer I ? I : never;
+    [InstanceK]: T extends new (...args: any[]) => infer I ? I : never;
 };
 
 export default function Singleton<T extends new (...args: any[] | undefined) => any>(targetClass: T) {
     return new Proxy(targetClass, {
         construct: (target: SingletonClass<T>, argumentsList, newTarget) => {
             if (target.prototype !== newTarget.prototype) return Reflect.construct(target, argumentsList, newTarget);
-            if (!target.instance) target.instance = new target(...argumentsList);
-            return target.instance;
+            if (!target[InstanceK]) target[InstanceK] = new target(...argumentsList);
+            return target[InstanceK];
         }
     });
 }
