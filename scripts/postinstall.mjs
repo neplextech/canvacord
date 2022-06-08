a66b1e89c3cfe6ec5563c632dba5856cbf92ba02
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2022-06-08 18:33:52 +0545

    download assets via postinstall script

---
+++ scripts/postinstall.mjs
@@ -0,0 +1,56 @@
+import fetch from "node-fetch";
+import fs from "node:fs";
+import chalk from "chalk";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const METADATA_URL = "https://raw.githubusercontent.com/CesiumLabs/canvacord/assets/datasrc.json";
+const ASSETS_DIR = `${dirname(fileURLToPath(import.meta.url))}/../assets`;
+
+if (!fs.existsSync(ASSETS_DIR)) await fs.promises.mkdir(ASSETS_DIR, { recursive: true });
+if (!fs.existsSync(`${ASSETS_DIR}/fonts`)) await fs.promises.mkdir(`${ASSETS_DIR}/fonts`, { recursive: true });
+if (!fs.existsSync(`${ASSETS_DIR}/images`)) await fs.promises.mkdir(`${ASSETS_DIR}/images`, { recursive: true });
+
+if (fs.existsSync(`${ASSETS_DIR}/meta.json`)) {
+    console.log(`${chalk.greenBright("[Canvacord]")} ${chalk.whiteBright(`Assets installation skipped since metadata is already available!`)}`);
+    process.exit();
+}
+
+console.log(`${chalk.yellowBright("[Canvacord]")} ${chalk.whiteBright("Downloading assets...")}`);
+
+const assetsMeta = await fetch(METADATA_URL, {
+    redirect: "follow"
+})
+    .then(res => res.json())
+    .then(data => {
+        if (!data?.data) throw new Error(`Corrupted assets source`);
+        return data;
+    })
+    .catch((e) => {
+        console.log(`${chalk.redBright("[Canvacord]")} ${chalk.whiteBright(`Failed to download assets:\n\t${chalk.redBright(e)}`)}`);
+    });
+
+if (!assetsMeta) {
+    console.log(`${chalk.redBright("[Canvacord]")} ${chalk.whiteBright(`Failed assets installation!`)}`);
+} else {
+    await fs.promises.writeFile(`${ASSETS_DIR}/meta.json`, JSON.stringify(assetsMeta));
+    console.log(`${chalk.greenBright("[Canvacord]")} ${chalk.whiteBright(`Successfully downloaded metadata!`)}`);
+
+    console.log(`${chalk.yellowBright("[Canvacord]")} ${chalk.whiteBright("Downloading images...")}`);
+    await Promise.all(assetsMeta.data.images.map(m => downloadAsset(m.url, m.name, true)));
+    console.log(`${chalk.yellowBright("[Canvacord]")} ${chalk.whiteBright("Downloading fonts...")}`);
+    await Promise.all(assetsMeta.data.fonts.map(m => downloadAsset(m.url, m.name, false)));
+
+    async function downloadAsset(url, name, image) {
+        const stream = await fetch(url).then(res => {
+            if (!res.ok || !res.body) throw new Error(`[HTTP${res.status}] Could not download ${url}!`);
+            return res.body;
+        });
+
+        const writer = stream.pipe(fs.createWriteStream(`${ASSETS_DIR}/${image ? "images" : "fonts"}/${name}`));
+
+        writer.on("finish", () => {
+            console.log(`${chalk.greenBright("[Canvacord]")} ${chalk.whiteBright(`Successfully downloaded ${chalk.cyanBright(name)}`)}`);
+        });
+    }
+}
