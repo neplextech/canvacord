ea3a16b006929cde31d1ddb23608650a6a9f0f31
KartikeSingh <gwk24yt@gmail.com>
2022-06-18 13:12:57 +0530

    Removed useless files

--- .js
+++
@@ -1,6 +0,0 @@
-const { writeFileSync } = require('fs');
-const x = require('./');
-
-x.ImageGen.changeMyMind("https://cdn.discordapp.com/attachments/983755478024728676/987337247214936184/unknown.png", "https://cdn.discordapp.com/attachments/814055106147385384/987563932518588466/unknown.png").then(v => {
-    writeFileSync("./i.png", v)
-})
