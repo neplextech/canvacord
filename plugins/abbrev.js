7e181cbd3caac26fa265dbb26a14737fde1e2724
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2022-09-12 17:28:55 +0545

    use intl formatter if available

--- plugins/abbrev.js
+++ plugins/abbrev.js
@@ -1,19 +1,24 @@
 module.exports = num => {
     if (!num || isNaN(num)) return "0";
     if (typeof num === "string") num = parseInt(num);
-    let decPlaces = Math.pow(10, 1);
-    var abbrev = ["K", "M", "B", "T"];
-    for (var i = abbrev.length - 1; i >= 0; i--) {
-        var size = Math.pow(10, (i + 1) * 3);
-        if (size <= num) {
-            num = Math.round((num * decPlaces) / size) / decPlaces;
-            if (num == 1000 && i < abbrev.length - 1) {
-                num = 1;
-                i++;
+
+    if (typeof Intl !== "undefined") {
+        return new Intl.NumberFormat("en", { notation: "compact" }).format(num);
+    } else {
+        let decPlaces = Math.pow(10, 1);
+        var abbrev = ["K", "M", "B", "T"];
+        for (var i = abbrev.length - 1; i >= 0; i--) {
+            var size = Math.pow(10, (i + 1) * 3);
+            if (size <= num) {
+                num = Math.round((num * decPlaces) / size) / decPlaces;
+                if (num == 1000 && i < abbrev.length - 1) {
+                    num = 1;
+                    i++;
+                }
+                num += abbrev[i];
+                break;
             }
-            num += abbrev[i];
-            break;
         }
+        return `${num}`;
     }
-    return `${num}`;
 };
