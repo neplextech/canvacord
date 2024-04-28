e07f82aef56f3a8c28035aa49010d2d4871412f7
twlite <46562212+twlite@users.noreply.github.com>
2024-04-28 14:53:12 +0545

    docs: fix issues

--- apps/docs/src/pages/guide/[topic]/_data.tsx
+++ apps/docs/src/pages/guide/[topic]/_data.tsx
@@ -1,55 +1,56 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
 export default function GuideData() {
-    return null;
+  return null;
 }
 
 export const pages = [
-{
-      name: "examples",
-      displayName: "Examples",
-      pages: [
-        {
-          name: "custom-image-generation.md",
-          displayName: "Custom Image Generation",
-          component: lazy(() => import('../_guides/Examples/custom-image-generation.md')),
-        },
-{
-          name: "gif-generation.md",
-          displayName: "Gif Generation",
-          component: lazy(() => import('../_guides/Examples/gif-generation.md')),
-        },
-{
-          name: "greetings-card.md",
-          displayName: "Greetings Card",
-          component: lazy(() => import('../_guides/Examples/greetings-card.md')),
-        },
-{
-          name: "image-generation.md",
-          displayName: "Image Generation",
-          component: lazy(() => import('../_guides/Examples/image-generation.md')),
-        },
-{
-          name: "leaderboard-image-generation.md",
-          displayName: "Leaderboard Image Generation",
-          component: lazy(() => import('../_guides/Examples/leaderboard-image-generation.md')),
-        },
-{
-          name: "rank-card.md",
-          displayName: "Rank Card",
-          component: lazy(() => import('../_guides/Examples/rank-card.md')),
-        }
-      ]
-    },
-{
-      name: "welcome",
-      displayName: "Welcome",
-      pages: [
-        {
-          name: "welcome.md",
-          displayName: "Welcome",
-          component: lazy(() => import('../_guides/Welcome/welcome.md')),
-        }
-      ]
-    },
+  {
+    name: "examples",
+    displayName: "Examples",
+    pages: [
+      {
+        name: "custom-image-generation.md",
+        displayName: "Custom Image Generation",
+        component: lazy(
+          () => import("../_guides/Examples/custom-image-generation.md")
+        ),
+      },
+      {
+        name: "greetings-card.md",
+        displayName: "Greetings Card",
+        component: lazy(() => import("../_guides/Examples/greetings-card.md")),
+      },
+      {
+        name: "image-generation.md",
+        displayName: "Image Generation",
+        component: lazy(
+          () => import("../_guides/Examples/image-generation.md")
+        ),
+      },
+      {
+        name: "leaderboard-image-generation.md",
+        displayName: "Leaderboard Image Generation",
+        component: lazy(
+          () => import("../_guides/Examples/leaderboard-image-generation.md")
+        ),
+      },
+      {
+        name: "rank-card.md",
+        displayName: "Rank Card",
+        component: lazy(() => import("../_guides/Examples/rank-card.md")),
+      },
+    ],
+  },
+  {
+    name: "welcome",
+    displayName: "Welcome",
+    pages: [
+      {
+        name: "welcome.md",
+        displayName: "Welcome",
+        component: lazy(() => import("../_guides/Welcome/welcome.md")),
+      },
+    ],
+  },
 ];
